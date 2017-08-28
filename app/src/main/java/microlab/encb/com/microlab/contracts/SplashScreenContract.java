4f888ca4c517f92af7c8549a1f155aded36db7ab
Gerardo Guzmán <gguzman@mimoni.com>
2017-08-28 07:06:57 -0500

    presenters

---
+++ app/src/main/java/microlab/encb/com/microlab/contracts/SplashScreenContract.java
@@ -0,0 +1,10 @@
+package microlab.encb.com.microlab.Interfaces;
+
+/**
+ * Created by Gerardo on 28/08/2017.
+ */
+
+public class SplashScreenContract {
+
+
+}
---
+++ app/src/main/java/microlab/encb/com/microlab/presenters/SplashScreenPresenter.java
@@ -0,0 +1,20 @@
+package microlab.encb.com.microlab.presenters;
+
+/**
+ * Created by Gerardo on 28/08/2017.
+ */
+
+public class SplashScreenPresenter {
+
+    public static interface View {
+        void
+    }
+
+
+    public static interface Action {
+    }
+
+
+    public static interface Repository {
+    }
+}
