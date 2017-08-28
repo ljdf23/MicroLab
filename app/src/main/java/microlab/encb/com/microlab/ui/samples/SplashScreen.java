c8d9d10f1ba4f6573e5068eadf2d045b958626b0
Gerardo Guzmán <gguzman@mimoni.com>
2017-08-28 07:05:10 -0500

    navigation bar & stepper

--- app/src/main/java/microlab/encb/com/microlab/ui/samples/SplashScreen.java
+++ app/src/main/java/microlab/encb/com/microlab/ui/samples/SplashScreen.java
@@ -1,8 +1,21 @@
 package microlab.encb.com.microlab.ui.samples;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import microlab.encb.com.microlab.R;
+
 /**
  * Created by Luis on 22/08/2017.
  */
 
-public class SplashScreen {
+public class SplashScreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
 }
